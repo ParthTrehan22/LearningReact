e1b65672bd161e01b464088e76dd00d80c8efa39
Trehan,Parth <Parth.Trehan@cerner.com>
2021-06-03 01:15:32 +0530

    Day6 (2/6/2021)

--- learning_react/src/Updating.js
+++ learning_react/src/Updating.js
@@ -10,27 +10,44 @@ export default class Updating extends Component {
             color: "Black"
         };
     }
-    changeColor = () =>{
-        this.setState({color:"White"})
+    // changeColor = () =>{
+    //     this.setState({color:"White"})
+    // }
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({model:"CLS 200"})
+        }, 5000);
     }
-    //Stops the updation from happening
-    shouldComponentUpdate(){
-        return false;
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        document.getElementById("div1").innerHTML = "The value before the update was"+prevState.model;
     }
+    componentDidUpdate(){
+        document.getElementById("div2").innerHTML = "The value after the update is"+this.state.model;
+    }
+    // //Stops the updation from happening
+    // shouldComponentUpdate(){
+    //     return false;
+    // }
     // static getDerivedStateFromProps(props, state){
     //     return {color: props.color};
     // }
     render() {
         return (
             <div>
-                <h1>The Garage contains</h1>
-                <p>Brand: {this.state.brand}</p>
-                <p>Model: {this.state.model}</p>
-                <p>Year: {this.state.year}</p>
-                <p>Color: {this.state.color}</p>
-                <button type="button" onClick = {this.changeColor}>
-                    Change Color
-                </button>
+                {/* <div id="div1">
+                    <h1>The Garage contains</h1>
+                    <p>Brand: {this.state.brand}</p>
+                    <p>Model: {this.state.model}</p>
+                    <p>Year: {this.state.year}</p>
+                    <p>Color: {this.state.color}</p>
+                    <button type="button" onClick = {this.changeColor}>
+                        Change Color
+                    </button>
+                </div> */}
+                <div id="div1">
+                </div>
+                <div id="div2">
+                </div>
             </div>
         )
     }
