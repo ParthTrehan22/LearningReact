6c8ebde591fa357d6459aefaf5ccb874b890d2b2
Trehan,Parth <Parth.Trehan@cerner.com>
2021-06-10 00:27:41 +0530

    Day10 (10/6/2021)

--- learning_react/src/Forms.js
+++ learning_react/src/Forms.js
@@ -5,20 +5,34 @@ export default class Forms extends Component {
         super(props);
         this.state = {username: ""};
     }
+    multipleValuesHandler = (event) =>{
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+    }
+    submitHandler = (event) =>{
+        event.preventDefault();
+        alert("You are submitting "+this.state.username);
+    }
     changeHandler = (event) => {
         this.setState({username: event.target.value});
     }
     render() {
+        let header = "";
+        if(this.state.username){
+            header = <h1>Hello: {this.state.username}</h1>;
+        }else{
+            header = "";
+        }
         return (
-            <form>
-                <h1>
-                    Hello: {this.state.username}
-                </h1>
+            <form onSubmit = {this.submitHandler}>
+                {header}
                 <p>Name:</p>
                 <input 
                 type="text"
                 onChange = {this.changeHandler}
                 ></input>
+                <input type="submit"></input>
             </form>
         )
     }
