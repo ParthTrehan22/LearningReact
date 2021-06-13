4f63be3ef753d340f040d318a45c9bcbf1f8dda9
Trehan,Parth <Parth.Trehan@cerner.com>
2021-06-14 01:51:37 +0530

    Day 12 (13/6/2021)

--- learning_react/src/Forms.js
+++ learning_react/src/Forms.js
@@ -3,20 +3,30 @@ import React, { Component } from 'react'
 export default class Forms extends Component {
     constructor(props){
         super(props);
-        this.state = {username: ""};
+        // this.state = {username: "", age: null, errorMessage: "", textAreaValue:"Hey welcome!!!", gender:"Male"};
+        this.state = {gender:'Male'};
     }
-    multipleValuesHandler = (event) =>{
-        let nam = event.target.name;
-        let val = event.target.value;
-        let gender = event.target.gender;
-        this.setState({[nam]: val});
-    }
-    submitHandler = (event) =>{
-        event.preventDefault();
-        alert("You are submitting "+this.state.username);
-    }
-    changeHandler = (event) => {
-        this.setState({username: event.target.value});
+    // multipleValuesHandler = (event) =>{
+    //     let nam = event.target.name;
+    //     let val = event.target.value;
+    //     let err = "";
+    //     if(nam === "age"){
+    //         if(val != "" && !Number(val)){
+    //             err = <strong>Age should be a number.</strong>
+    //         }
+    //     }
+    //     this.setState({errorMessage: err});
+    //     this.setState({[nam]: val});
+    // }
+    // submitHandler = (event) =>{
+    //     event.preventDefault();
+    //     alert("You are submitting "+this.state.username);
+    // }
+    // changeHandler = (event) => {
+    //     this.setState({username: event.target.value});
+    // }
+    handleChange = (event) => {
+        this.setState({gender : event.target.value});
     }
     render() {
         // let header = "";
@@ -27,17 +37,12 @@ export default class Forms extends Component {
         // }
         return (
             <form>
+                {/* <textarea value = {this.state.textAreaValue}></textarea>
                 <h1>Hello {this.state.username} and your age is {this.state.age} and your gender is {this.state.gender}</h1>
                 <p>Enter your name:</p>
                 <input
                     type='text'
                     name='username'
-                    onChange={this.multipleValuesHandler}
-                ></input>
-                <p>Enter your gender:</p>
-                <input
-                    type='text'
-                    name='gender'
                     onChange={this.multipleValuesHandler}
                 ></input>
                 <p>Enter your age:</p>
@@ -47,6 +52,12 @@ export default class Forms extends Component {
                     onChange={this.multipleValuesHandler}
                 >
                 </input>
+                {this.state.errorMessage} */}
+                <select value={this.state.gender} onChange={this.handleChange}>
+                    <option value='Male'>Male</option>
+                    <option value='Female'>Female</option>
+                    <option value='Other'>Other</option>
+                </select>
             </form>
             // <form onSubmit = {this.submitHandler}>
             //     {header}
